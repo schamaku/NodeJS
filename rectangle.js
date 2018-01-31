323d701a78f6e97d07da326dad9b1963df8d0a22
nidhi reddy <nidhireddy@nidhis-MBP.fios-router.home>
2018-01-30 19:39:01 -0500

    Node callbacks

--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,29 @@
-exports.perimeter = (x,y) => (2*(x+y));
-exports.area = (x,y) => (x*y); 
+module.exports = (x,y,callback) => {
+	if(x<=0 || y<=0){
+		setTimeout(() => 
+			callback(new Error("dimensions must be positive"), 
+				null),
+			2000);
+		console.log("dimensions must be greater than 0 l:" + x + " and b:" + y);
+	}
+	else{
+		setTimeout(() => 
+			callback(null, 
+				{
+					perimeter :() => (2*(x+y)),
+                    area :() => (x*y)
+				}),
+			2000);
+
+	
+	}
+}
+
+
+
+
+
+
+
+perimeter = (x,y) => (2*(x+y));
+area = (x,y) => (x*y); 
